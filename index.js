c0e94482396fd33ac042387dfeae2aa74fe72b0a
James Stuart <james.stuart@gmail.com>
2020-08-06 08:58:50 -0400

    added todo taskpaper and linting

--- index.js
+++ index.js
@@ -2,37 +2,25 @@
 
 // Load the facebook data set.
 
-
-const csv = require('csv-parser');
-const fs = require('fs');
-
-
+const csv = require("csv-parser");
+const fs = require("fs");
 
 function loadCSV(filename) {
   const rows = [];
   let rowsRead = 0;
 
   return new Promise((resolve, reject) => {
-  fs.createReadStream(filename)
-    .pipe(csv())
-    .on('data', (data) => {
-      rowsRead += 1;
-      if (rowsRead % 10000 === 0) { // every 10000 rows, output a dot
-        process.stdout.write('.');
-      }
-
-     rows.push(data);
-    })
-    .on('error', (error) => reject(error))
-    .on('end', () => resolve(rows))
+    fs.createReadStream(filename)
+      .pipe(csv())
+      .on("data", (data) => rows.push(data))
+      .on("error", (error) => reject(error))
+      .on("end", () => resolve(rows));
   });
-
 }
 
-
-loadCSV('./datasets/Backpack.csv')
-  .then ((results) => {
-      console.log("\nData loaded!");
-      console.log(results.length);
-
+// or ./datasets/Arrests.csv
+// get more small datasets at https://vincentarelbundock.github.io/Rdatasets/datasets.html
+loadCSV("./datasets/Backpack.csv").then((results) => {
+  console.log("\nData loaded!");
+  console.log(results.length);
 });
