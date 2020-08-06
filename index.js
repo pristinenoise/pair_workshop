8ce5e0fcd873ec45dda18fe1502a46576298abae
James Stuart <james.stuart@gmail.com>
2020-08-06 09:47:23 -0400

    switch to datalib

--- index.js
+++ index.js
@@ -1,42 +1,12 @@
-// https://simplestatistics.org/docs/
+const dl = require("datalib");
 
-// Load the facebook data set.
+const data = dl.csv("./datasets/Backpack.csv");
 
-const csv = require("csv-parser");
-const fs = require("fs");
-const data
-const Statistics = require("statistics.js");
+// api reference: https://github.com/vega/datalib/wiki/API-Reference
+// examples: https://github.com/vega/datalib/wiki/Examples
+console.log("First 5 rows:");
+console.log(dl.format.table(data, { limit: 5 }));
 
-function loadCSV(filename) {
-  const rows = [];
-
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(filename)
-      .pipe(csv())
-      .on("data", (data) => rows.push(data))
-      .on("error", (error) => reject(error))
-      .on("end", () => resolve(rows));
-  });
-}
-
-// get more small datasets at https://vincentarelbundock.github.io/Rdatasets/datasets.html
-loadCSV("./datasets/Backpack.csv").then((results) => {
-  console.log("\nData loaded!");
-  console.log(`${results.length} rows`);
-  console.log(`First row: ${JSON.stringify(results[0])}`);
-
-  const columns = {
-    BackpackWeight: "metric",
-    BodyWeight: "metric",
-    Ratio: "interval",
-    BackProblems: "nominal",
-    Major: "nominal",
-    Year: "nominal",
-    Gender: "nominal",
-    Status: "nominal",
-    Units: "interval",
-  };
-  const stats = new Statistics(results, columns);
-
-  const 
-});
+// console.log(`Number of people by major: `);
+// const countByMajor = dl.groupby("Major").count().execute(data);
+// console.log(dl.format.table(countByMajor));
